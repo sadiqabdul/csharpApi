99a984772f68e3bb74e9ccf77da50770f804e12a
sadiqabdul <38528668+sadiqabdul@users.noreply.github.com>
2024-10-17 11:26:20 +0100

    first commit

--- obj/Debug/net8.0/AuthAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/AuthAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab0473cf8405df5327f599c9911d0968b47e351")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
